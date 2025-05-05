f67d40e43b1eb2b64382bfde7dbe0d468b0bf224
Alej-o <lejour.agathe@outlook.fr>
2025-05-05 18:06:04 +0200

    globalbackground w/o mouse interaction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { Geist, Geist_Mono,Domine } from "next/font/google";
 import "./globals.css";
+import GlobalBackground from "../components/GlobalBackground";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +32,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-  <body className={`${geistSans.variable} ${geistMono.variable} ${domine.variable} bg-orange-50 text-zinc-900 overflow-x-hidden`}>
+  <body className={`${geistSans.variable} ${geistMono.variable} ${domine.variable}`}>
+  <GlobalBackground />
     <Header />
     <main>
       {children}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
-import { HeroBackground } from "../components/HeroBackground";
+// import { Canvas } from "@react-three/fiber";
+// import { HeroBackground } from "../components/HeroBackground";
 
 export default function Hero() {
   return (
     <section className="relative w-full h-screen overflow-hidden">
    
-      <div className="absolute inset-0 z-0 pointer-events-auto">
+      {/* <div className="absolute inset-0 z-0 pointer-events-auto">
         <Canvas
           orthographic
           camera={{ position: [0, 0, 1], zoom: 1 }}
-          className="w-full h-full"
+          className="w-full h-full" 
         >
           <HeroBackground />
         </Canvas>
-      </div>
+      </div> */}
       <div className="relative z-10 pointer-events-none flex flex-col items-center justify-center h-full text-center">
         <h1 className="text-7xl font-serif text-white mt-50">Agathe Lejour</h1>
         <p className="mt-4 text-xl font-serif text-white">Développeuse web et mobile</p>
