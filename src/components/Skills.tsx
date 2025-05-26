1daf3f060638873db7a0e6700da4f00175e6910a
Alej-o <lejour.agathe@outlook.fr>
2025-05-26 16:58:01 +0200

    animation paragraph

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,11 @@
 // app/components/Skills.tsx
+'use client'
+import AnimatedParagraph from './AnimatedParagraph'
 export default function Skills() {
     return (
-      <section className="py-8 w-full bg-black">
-        <h2 className="text-5xl text-center text-beige font-milk-honey">À propos</h2>
-        {/* Ici on affichera les compétences plus tard */}
-      </section>
+       <section className="bg-black text-beige font-milk-honey py-20">
+      <AnimatedParagraph text="Hello, moi c’est Agathe ! Développeuse web fullstack, passionnée par la créativité sous toutes ses formes — qu’il s’agisse d’animations front-end ou d’architectures back-end bien pensées. J’aime autant concevoir des interfaces vivantes que structurer ce qui les anime. Le code est mon terrain de jeu, l’émotion mon objectif." />
+    </section>
     );
   }
   
